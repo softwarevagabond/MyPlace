7c582d81bd1dd22add251bcbf454fc67e88e7642
ronithomas1 <ronithomas@gmail.com>
2015-08-05 19:15:29 +0530

    Changes done until proxies

---
+++ MyPlace.ServiceHost.Tests/ServiceAccessTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ServiceModel;
+using MyPlace.Business.Contracts;
+
+namespace MyPlace.ServiceHost.Tests
+{
+    [TestClass]
+    public class ServiceAccessTests
+    {
+        [TestMethod]
+        public void test_security_manager_as_service()
+        {
+            ChannelFactory<ISecurityService> channelFactory =
+                new ChannelFactory<ISecurityService>("");
+
+            ISecurityService proxy = channelFactory.CreateChannel();
+
+            (proxy as ICommunicationObject).Open();
+
+          var res=  proxy.GetResident("U10000");
+
+            channelFactory.Close();
+        }
+    }
+}
